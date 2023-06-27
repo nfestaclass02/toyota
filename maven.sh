6f222ff12aaa3c2798e9f1a2188fb238d397dd00
festus nkeng <festusnkeng@yahoo.com>
2023-06-27 01:12:17 +0000

    we modified the our buld file

--- maven.sh
+++ maven.sh
@@ -2,3 +2,5 @@
 echo 'getting ready to engage in a full time/E2E Automation'
 echo 'I am sure this is going to be interesting'
 echo 'All the same we are ready for it'
+echo 'we are getting used to it'
+echo 'surely this shopuld go as planned' 
