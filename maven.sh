858d7f657f86b03f376429a8af093153bb5f6452
festus nkeng <festusnkeng@yahoo.com>
2023-06-27 01:17:37 +0000

    added a line of code in our build file

--- maven.sh
+++ maven.sh
@@ -2,3 +2,4 @@
 echo 'getting ready to engage in a full time/E2E Automation'
 echo 'I am sure this is going to be interesting'
 echo 'All the same we are ready for it'
+echo 'we are getting in a matter of time'
