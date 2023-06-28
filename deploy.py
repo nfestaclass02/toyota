d66495976e0c8e6e63042606be082109bd7626a0
festus nkeng <festusnkeng@yahoo.com>
2023-06-28 00:56:14 +0000

    ignored usernfo_dev and modified deployment file

--- deploy.py
+++ deploy.py
@@ -4,6 +4,7 @@ print(we will be automating all Toyota client tasks)
 print('Hello, I'm sure you all are getting it')
 print('testing fetch')
 print('tested pull and comfirm')
-apps deployment2
-adding apps deployment3
-including apps deployment4
+#apps deployment2
+#adding apps deployment3
+#including apps deployment
+
