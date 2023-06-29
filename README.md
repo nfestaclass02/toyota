5be54f6364ee4943a97f7ed02d14918a9c45266a
festus nkeng <festusnkeng@yahoo.com>
2023-06-29 18:49:55 +0000

    added new file and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # toyota_project
+#this file is user to monitor all client application servers
---
+++ monitor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 'alert server status'
+df -f
+free -m
+date
+echo 'server status is as incated above'
