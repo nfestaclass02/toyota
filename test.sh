55c4b79a91da7da469c58e5d6537170c0dd8f8a2
nfestaclass02 <nfestatech@gmail.com>
2023-07-07 02:17:19 +0000

    correction

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@ testing our git cherry-pick
 add a line for testing
 checking the push branch to remote repo
 now checking ssh push/access to remote repo
-add a line of code
+add a line of code 
+checking commit id tagging
+and all planning to rull back/modify already committed changes to our localrepo
